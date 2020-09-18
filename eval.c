a77d1f32e6f460a70d680c5471cdd85a2bf8e675
jimd1989 <jamesdalrymple@protonmail.com>
2020-09-18 16:24:10 +0900

    set wave func

--- eval.c
+++ eval.c
@@ -16,7 +16,8 @@ static void boundIError(int, int, int);
 static void boundFError(float, float, float);
 static OutResult noteOn(ArgVal *, Out *);
 static OutResult noteOff(ArgVal *, Out *);
-static OutResult envSet(ArgVal *as, Out *o);
+static OutResult setEnv(ArgVal *, Out *);
+static OutResult setWave(ArgVal *, Out *);
 static OutResult echo(ArgVal *, Parse *, Out *);
 
 static bool
@@ -65,13 +66,23 @@ noteOff(ArgVal *as, Out *o) {
 }
 
 static OutResult
-envSet(ArgVal *as, Out *o) {
+setEnv(ArgVal *as, Out *o) {
   int16_t size = sizeof(uint8_t) + sizeof(int) + sizeof(float) + sizeof(float);
   _O(writeHead(o, size))
   _O(writeByte(o, (uint8_t)as[0].i))
   _O(writeInt(o, as[1].i));
   _O(writeFloat(o, as[2].f));
   _O(writeFloat(o, as[3].f));
+  return OUT_SUCCESS;
+}
+
+static OutResult
+setWave(ArgVal *as, Out *o) {
+  int16_t size = sizeof(uint8_t) + sizeof(int) + sizeof(int);
+  _O(writeHead(o, size))
+  _O(writeByte(o, (uint8_t)as[0].i))
+  _O(writeInt(o, as[1].i))
+  _O(writeEnum(o, as[2].s))
   return OUT_SUCCESS;
 }
 
@@ -100,24 +111,24 @@ eval(Parse *p, Out *o) {
   r =
     f == F_NOTE_ON      ? noteOn(as++, o)  :
     f == F_NOTE_OFF     ? noteOff(as++, o) :
-    f == F_ATTACK       ? envSet(as, o)    :
-    f == F_ATTACK_WAVE  ? true             :
-    f == F_DECAY        ? envSet(as, o)    :
-    f == F_DECAY_WAVE   ? true             :
+    f == F_ATTACK       ? setEnv(as, o)    :
+    f == F_ATTACK_WAVE  ? setWave(as, o)   :
+    f == F_DECAY        ? setEnv(as, o)    :
+    f == F_DECAY_WAVE   ? setWave(as, o)   :
     f == F_ENV_ASSIGN   ? true             :
     f == F_ECHO         ? echo(as, p, o)   :
-    f == F_KEY_CURVE    ? true             :
+    f == F_KEY_CURVE    ? setWave(as, o)   :
     f == F_LOUDNESS     ? true             :
     f == F_AMPLITUDE    ? true             :
     f == F_MODULATE     ? true             :
     f == F_ENV_LOOP     ? true             :
     f == F_PITCH        ? true             :
     f == F_QUIT         ? true             :
-    f == F_RELEASE      ? envSet(as, o)    :
-    f == F_RELEASE_WAVE ? true             :
-    f == F_SUSTAIN      ? envSet(as, o)    :
+    f == F_RELEASE      ? setEnv(as, o)    :
+    f == F_RELEASE_WAVE ? setWave(as, o)   :
+    f == F_SUSTAIN      ? setEnv(as, o)    :
     f == F_TOUCH        ? true             :
     f == F_TUNE         ? true             :
-    f == F_WAVE         ? true             : false;
+    f == F_WAVE         ? setWave(as,o)    : false;
   return r;
 }
